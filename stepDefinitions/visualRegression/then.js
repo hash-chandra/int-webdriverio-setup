6e3498be47f2ccf664fec25adf218d01a9e035c9
chandrashekhar chanagonda <chandrashekhar.chanagonda@moduscreate.com>
2024-03-01 21:32:52 +0530

    updated baseline image

--- stepDefinitions/visualRegression/then.js
+++ stepDefinitions/visualRegression/then.js
@@ -1,6 +1,10 @@
 import { Then } from '@cucumber/cucumber';
 
+const locators = {
+    dashboardPanel: "ul[class='oxd-main-menu']"
+};
+
 Then(/^I should see the Admin homepage screen$/, async () => {
     await browser.pause(5000);
-    await expect(await browser.checkScreen('adminHomePage')).toEqual(0);
+    await expect(await browser.checkElement( await $(locators.dashboardPanel),'adminHomePage')).toEqual(0);
 });
